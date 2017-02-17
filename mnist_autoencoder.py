07a7c0eeede62b20ea32bb27787daf5f90991967
nlunscher <nolan_lunscher@hotmail.com>
2017-02-17 18:11:54 -0500

    seems to work now

--- mnist_autoencoder.py
+++ mnist_autoencoder.py
@@ -67,44 +67,44 @@ with tf.Graph().as_default():
     x_image = tf.reshape(x, [-1, 28, 28, 1]) # 28x28
     print x_image
 
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
+    W_conv1 = weight_variable([5, 5, 1, 24])
+    b_conv1 = bias_variable([24])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1, name="conv1")
     print h_conv1
     h_pool1 = max_pool_2x2(h_conv1) # 14x14
     print h_pool1
 
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
+    W_conv2 = weight_variable([5, 5, 24, 32])
+    b_conv2 = bias_variable([32])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2, name="conv2")
     print h_conv2
     h_pool2 = max_pool_2x2(h_conv2) # 7x7
     print h_pool2
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*32])
 
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
+    W_fc1 = weight_variable([7 * 7 * 32, 512])
+    b_fc1 = bias_variable([512])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1, name="fc1")
     print h_fc1
 
-    W_fc2 = weight_variable([1024, 512])
-    b_fc2 = bias_variable([512])
+    W_fc2 = weight_variable([512, 128])
+    b_fc2 = bias_variable([128])
     h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2, name="fc2") # center representation
     print h_fc2
 
-    W_fc3 = weight_variable([512, 7 * 7 * 64])
-    b_fc3 = bias_variable([7 * 7 * 64])
+    W_fc3 = weight_variable([128, 7 * 7 * 32])
+    b_fc3 = bias_variable([7 * 7 * 32])
     h_fc3 = tf.nn.relu(tf.matmul(h_fc2, W_fc3) + b_fc3, name="fc3")
     print h_fc3
 
-    h_fc3_2d = tf.reshape(h_fc3, [-1, 7, 7, 64]) # 7x7
+    h_fc3_2d = tf.reshape(h_fc3, [-1, 7, 7, 32]) # 7x7
     print h_fc3_2d
 
     h_unpool1 = unpool_2x2(h_fc3_2d) # 14x14
     print h_unpool1
-    W_dconv1 = weight_variable([5, 5, 32, 64])
-    b_dconv1 = bias_variable([32])
+    W_dconv1 = weight_variable([5, 5, 24, 32])
+    b_dconv1 = bias_variable([24])
     osize_dconv1 = h_pool1.get_shape().as_list()
     osize_dconv1[0] = batch_size
     h_dconv1 = tf.nn.relu(conv2d_transpose(h_unpool1, W_dconv1, osize_dconv1)
@@ -113,11 +113,11 @@ with tf.Graph().as_default():
 
     h_unpool2 = unpool_2x2(h_dconv1) # 28x28
     print h_unpool2
-    W_dconv2 = weight_variable([5, 5, 1, 32])
+    W_dconv2 = weight_variable([5, 5, 1, 24])
     b_dconv2 = bias_variable([1])
     osize_dconv2 = x_image.get_shape().as_list()
     osize_dconv2[0] = batch_size
-    h_dconv2 = tf.nn.relu(conv2d_transpose(h_unpool2, W_dconv2, osize_dconv2)
+    h_dconv2 = tf.nn.sigmoid(conv2d_transpose(h_unpool2, W_dconv2, osize_dconv2)
                         + b_dconv2, name="dconv2")
     print h_dconv2
 
@@ -128,7 +128,7 @@ with tf.Graph().as_default():
     loss = tf.reduce_sum(tf.square(x_image - y_image))
     ########################################################################
 
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+    train_step = tf.train.AdamOptimizer(1e-6).minimize(loss)
 
 
     saver = tf.train.Saver(max_to_keep=10000)
@@ -139,10 +139,10 @@ with tf.Graph().as_default():
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
 
-        # cv2.namedWindow("Real Image", cv2.WINDOW_NORMAL)
-        # cv2.resizeWindow("Real Image", 200, 200)
-        # cv2.namedWindow("CNN Image", cv2.WINDOW_NORMAL)
-        # cv2.resizeWindow("CNN Image", 200, 200)
+        cv2.namedWindow("Real Image", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("Real Image", 200, 200)
+        cv2.namedWindow("CNN Image", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("CNN Image", 200, 200)
 
         training_start_time = datetime.datetime.now()
         print "Starting, training for", train_iterations, "iterations"
@@ -156,13 +156,20 @@ with tf.Graph().as_default():
                 iloss, ix_image, iy_image = sess.run([loss, x_image, y_image], feed_dict={x:batch[0]})
                 print "step",i, "loss", iloss, "duration", datetime.datetime.now() - start_time
 
-                # cv2.imshow("Real Image", ix_image[0])
-                # key = cv2.waitKey(10)
-                # cv2.imshow("CNN Image", iy_image[0])
-                # key = cv2.waitKey(10)
+                print "Real Image", np.max(ix_image[0]), np.min(ix_image[0])
+                print "CNN Image", np.max(iy_image[0]), np.min(iy_image[0]) 
+                cv2.imshow("Real Image", ix_image[0])
+                key = cv2.waitKey(10)
+                cv2.imshow("CNN Image", iy_image[0])
+                key = cv2.waitKey(10)
+                
                 if i % 1000 == 0 or i == 1:
                     cv2.imwrite("saved_images/" + str(i) + "_real_im.tif", ix_image[0]*255)
                     cv2.imwrite("saved_images/" + str(i) + "_cnn_im.tif", iy_image[0]*255)
+
+            if i % 100000 == 0:
+                save_path = saver.save(sess, "saved_models/mnist_auto.tfrecords")
+                print ("Saved model as: %s" % save_path)
             # else:
             #     print i
 
@@ -175,7 +182,7 @@ with tf.Graph().as_default():
         # print "Testing"
         # print("test accuracy %g"%accuracy.eval(feed_dict={
         #       x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-        save_path = saver.save(sess, "mnist_auto.tfrecords")
+        save_path = saver.save(sess, "saved_models/mnist_auto_final.tfrecords")
         print ("Saved model as: %s" % save_path)
 
 
