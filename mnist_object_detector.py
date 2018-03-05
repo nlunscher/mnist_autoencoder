ce17af29a90433ae502dfd650544a8875861a868
nlunscher <nolan_lunscher@hotmail.com>
2018-03-05 08:31:10 -0500

    maybe now?

--- mnist_object_detector.py
+++ mnist_object_detector.py
@@ -5,6 +5,7 @@ import cv2
 import random
 import os
 import datetime as dt
+import threading
 
 class Network:
     def __init__(self, image, im_size, num_bb, is_train):
@@ -63,10 +64,12 @@ class MNIST_OD_Trainer:
         print("Number of files: %d" % len(self.files))
 
         self.batch_size = 16
+        self.pos_net_ratio = 1 #:1
+        self.min_conf = 0.4
 
         self.sub_im_size = [28, 28]
         self.im_size = [128, 128]
-        self.show_size = (512, 512)
+        self.show_size = (256, 256)
 
         self.bb_size = {8:14, 16:6, 32:2}
         self.bb_shapes = np.array([[.75, .75], 
@@ -84,6 +87,16 @@ class MNIST_OD_Trainer:
 
         self.sub_scale_options = np.arange(0.9, 4, 0.1)
         self.sub_count = range(1,5)
+
+        self.im_batch = []
+        self.bb_batch = []
+        self.enc_bb_batch = [[],[],[]]
+
+        self.im_batch_loading = []
+        self.bb_batch_loading = []
+        self.enc_bb_batch_loading = [[],[],[]]
+
+        self.STOP = False
 
         self.debug = {}
 
@@ -240,12 +253,80 @@ class MNIST_OD_Trainer:
         enc_bb_batch[2] = np.asarray(enc_bb_batch[2])
         return np.asarray(im_batch), np.asarray(bb_batch), enc_bb_batch
 
+    def load_batch(self):
+        im_batch = []
+        bb_batch = []
+        enc_bb_batch = [[], [], []]
+        for i in range(self.batch_size):
+            im, bbs = self.make_image()
+
+            encoded_bbs8 = self.encode_2d_bb(bbs, 8)
+            encoded_bbs16 = self.encode_2d_bb(bbs, 16)
+            encoded_bbs32 = self.encode_2d_bb(bbs, 32)
+
+            im_batch.append(im)
+            bb_batch.append(bbs)
+            enc_bb_batch[0].append(encoded_bbs8)
+            enc_bb_batch[1].append(encoded_bbs16)
+            enc_bb_batch[2].append(encoded_bbs32)
+
+        enc_bb_batch[0] = np.asarray(enc_bb_batch[0])
+        enc_bb_batch[1] = np.asarray(enc_bb_batch[1])
+        enc_bb_batch[2] = np.asarray(enc_bb_batch[2])
+
+        self.im_batch = np.asarray(im_batch)
+        self.bb_batch = np.asarray(bb_batch)
+        self.enc_bb_batch = enc_bb_batch
+
+    def load_single(self, index):
+        im, bbs = self.make_image()
+
+        encoded_bbs8 = self.encode_2d_bb(bbs, 8)
+        encoded_bbs16 = self.encode_2d_bb(bbs, 16)
+        encoded_bbs32 = self.encode_2d_bb(bbs, 32)
+
+        self.im_batch_loading[index] = im
+        self.bb_batch_loading[index] = bbs
+        self.enc_bb_batch_loading[0][index] = encoded_bbs8
+        self.enc_bb_batch_loading[1][index] = encoded_bbs16
+        self.enc_bb_batch_loading[2][index] = encoded_bbs32
+
+    def parallel_load_batch(self):
+        self.im_batch_loading = [None] * self.batch_size
+        self.bb_batch_loading = [None] * self.batch_size
+        self.enc_bb_batch_loading = [[None] * self.batch_size, [None] * self.batch_size, [None] * self.batch_size]
+        
+        loaders = []
+        for i in range(self.batch_size):
+            loader = threading.Thread(target=self.load_single, args=(i,))
+            loader.start()
+            loaders.append(loader)
+        for loader in loaders:
+            loader.join()
+
+        self.enc_bb_batch_loading[0] = np.asarray(self.enc_bb_batch_loading[0])
+        self.enc_bb_batch_loading[1] = np.asarray(self.enc_bb_batch_loading[1])
+        self.enc_bb_batch_loading[2] = np.asarray(self.enc_bb_batch_loading[2])
+
+        self.im_batch = np.asarray(self.im_batch_loading)
+        self.bb_batch = np.asarray(self.bb_batch_loading)
+        self.enc_bb_batch = self.enc_bb_batch_loading
+
+    def copy_batch(self):
+        return self.im_batch[:], self.bb_batch[:], self.enc_bb_batch[:]
+
+    def thread_activity(self, dataset_type = "Train"):
+        if not self.STOP:
+            self.STOP = True
+            self.parallel_load_batch()
+            self.STOP = False
+
     def draw_bb(self, im, bbs, conf, color=(0,1,0)):
         new_im = np.copy(im)
         # print len(bbs)
         for i in range(len(bbs)):
             # print(bbs[i])
-            if conf[i] >= 0.5:
+            if conf[i] >= self.min_conf:
                 bb = self.bb_center2corner(bbs[i])
                 # print bb
                 cv2.rectangle(new_im, (int(bb[0][0] * im.shape[0]), int(bb[0][1] * im.shape[1])), 
@@ -336,6 +417,7 @@ class MNIST_OD_Trainer:
                         r_h = bb[3] - d_bb[3]
                         # print [r_x, r_y, r_w, r_h]
                         gt[p_y, p_x, b*6 + 1] = 1.0
+                        gt[p_y, p_x, b*6 + 0] = 0.0
                         gt[p_y, p_x, b*6 + 2:b*6 + 6] = [r_x, r_y, r_w, r_h]
 
 
@@ -362,7 +444,10 @@ class MNIST_OD_Trainer:
 
 
                     else:
-                        gt[p_y, p_x, b*6 + 0] = 1.0 # is class 0 (background)
+                        if gt[p_y, p_x, b*6 + 1] != 1.0:
+                            gt[p_y, p_x, b*6 + 0] = 1.0 # is class 0 (background)
+
+                    # print p_y, p_x, b, gt[p_y, p_x, b*6:b*6+2]
 
 
         # print
@@ -418,7 +503,7 @@ class MNIST_OD_Trainer:
                     # print [r_x, r_y, r_w, r_h] # [-0.23671875000000001, 0.70625000000000004, -0.22031249999999999, -0.1875]
 
                     soft = self.np_softmax(encoded_bb[y, x, b*6:b*6 + 1 + 1])
-                    print encoded_bb[y, x, b*6:b*6 + 2], soft
+                    # print encoded_bb[y, x, b*6:b*6 + 2], soft
 
                     bbs.append([c_x, c_y, c_w, c_h])
                     confs.append(soft[1])
@@ -520,9 +605,10 @@ class MNIST_OD_Trainer:
         print neg_xe_vec
         print neg_bb_vec
 
-        num_pos = tf.cast(tf.reduce_sum(pos_mask), tf.int32)
-        num_neg = tf.minimum(tf.maximum(num_pos * 3, 3), vec_len) # take at least 3 negative
-        self.debug[scale] = num_neg
+        num_pos = tf.cast(tf.reduce_sum(pos_mask,axis=1), tf.int32)
+        num_neg = tf.reduce_max(tf.minimum(tf.maximum(num_pos * self.pos_net_ratio, 1), vec_len)) # take at least 3 negative
+        print num_pos
+        print num_neg
 
         v_top_neg, i_top_neg = tf.nn.top_k(neg_xe_vec, k=num_neg)
         v_top_min = tf.reduce_min(v_top_neg)
@@ -533,11 +619,13 @@ class MNIST_OD_Trainer:
         print top_neg_xe_vec
         print top_neg_bb_vec
 
-        pos_loss = tf.reduce_mean(pos_xe_vec) + tf.reduce_mean(pos_bb_vec)
+        pos_loss = tf.reduce_mean((4*tf.reduce_sum(pos_xe_vec, axis=1) + tf.reduce_sum(pos_bb_vec, axis=1)) / tf.cast(tf.maximum(num_pos, 1), tf.float32))
         print pos_loss
 
-        neg_loss = tf.reduce_mean(top_neg_xe_vec) + tf.reduce_mean(top_neg_bb_vec)
+        neg_loss = tf.reduce_mean((4*tf.reduce_sum(top_neg_xe_vec, axis=1)) /  tf.cast(tf.maximum(num_neg, 1), tf.float32))
         print neg_loss
+
+        self.debug[scale] = tf.cast(num_pos, tf.float32)
 
         loss = pos_loss + neg_loss
         print loss
@@ -576,10 +664,19 @@ class MNIST_OD_Trainer:
         cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
         cv2.resizeWindow("Image", self.show_size[0]*4, self.show_size[1]*3)
 
-        train_iterations = 1000
+        train_iterations = 10000
         start_time = dt.datetime.now()
+        batch_getter = threading.Thread(target=self.thread_activity, args=())
+        batch_getter.start()
         for i in range(train_iterations):
-            ims, bbs, enc_bbs = self.get_batch()
+            # ims, bbs, enc_bbs = self.get_batch()
+            batch_getter.join()
+            if self.STOP:
+                print "STOPPING TRAIN!!"
+                break
+            ims, bbs, enc_bbs = self.copy_batch()
+            batch_getter = threading.Thread(target=self.thread_activity, args=("Train",))
+            batch_getter.start()
 
             # d8,d16,d32 = self.sess.run([self.debug[8], self.debug[16], self.debug[32]], 
             #                                         feed_dict={self.in_image:ims, 
@@ -596,38 +693,42 @@ class MNIST_OD_Trainer:
                                                                 self.gt_image16:enc_bbs[1],
                                                                 self.gt_image32:enc_bbs[2]})
 
-            if i % 10 == 0 or i == train_iterations-1:
+            if i % 100 == 0 or i == train_iterations-1:
                 print i, "|", loss, "|", dt.datetime.now() - start_time
                 show_im2 = self.make_show_image(ims[0], bbs[0], o8[0], o16[0], o32[0])
                 cv2.imshow("Image", show_im2)
                 cv2.waitKey(10)
+                cv2.imwrite("od_ims/" + str(i).zfill(9) + ".png", show_im2)
 
     def make_show_image(self, image, gt_bbs, enc_bbs8, enc_bbs16, enc_bbs32):
 
-        # print enc_bbs8.shape
+        # print np.max(image), np.min(image)
 
         image = cv2.cvtColor(cv2.resize(image, self.show_size), cv2.COLOR_GRAY2RGB)
         conf_gt = np.ones(len(gt_bbs), np.float32)
         image_gt = self.draw_bb(image, gt_bbs, conf_gt, (0,1,1))
-
+# 
         gt_enc_bbs8 = self.encode_2d_bb(gt_bbs, 8)
         gt_enc_bbs16 = self.encode_2d_bb(gt_bbs, 16)
         gt_enc_bbs32 = self.encode_2d_bb(gt_bbs, 32)
 
         decoded8, confs8 = self.decode_2d_bb(gt_enc_bbs8, 8)
+        confs8 = np.ones_like(confs8)
         gt_image_dec0 = self.draw_bb(image, decoded8, confs8)
         decoded16, confs16 = self.decode_2d_bb(gt_enc_bbs16, 16)
+        confs16 = np.ones_like(confs16)
         gt_image_dec = self.draw_bb(image, decoded16, confs16)
         decoded32, confs32 = self.decode_2d_bb(gt_enc_bbs32, 32)
+        confs32 = np.ones_like(confs32)
         gt_image_dec2 = self.draw_bb(image, decoded32, confs32)
-        gt_image_dec_total = self.draw_bb(image, decoded8 + decoded16 + decoded32, confs8 + confs16 + confs32, (1,1,0))
+        gt_image_dec_total = self.draw_bb(image, decoded8 + decoded16 + decoded32, np.concatenate([confs8, confs16, confs32]))
 
         decoded8, confs8 = self.decode_2d_bb(enc_bbs8, 8)
-        image_dec0 = self.draw_bb(image, decoded8, confs8)
+        image_dec0 = self.draw_bb(image, decoded8, confs8, (1,1,0))
         decoded16, confs16 = self.decode_2d_bb(enc_bbs16, 16)
-        image_dec = self.draw_bb(image, decoded16, confs16)
+        image_dec = self.draw_bb(image, decoded16, confs16, (1,1,0))
         decoded32, confs32 = self.decode_2d_bb(enc_bbs32, 32)
-        image_dec2 = self.draw_bb(image, decoded32, confs32)
+        image_dec2 = self.draw_bb(image, decoded32, confs32, (1,1,0))
         image_dec_total = self.draw_bb(image, decoded8 + decoded16 + decoded32, confs8 + confs16 + confs32, (1,1,0))
 
         spacer = np.zeros((self.show_size[0], self.show_size[1], 3))
@@ -636,6 +737,7 @@ class MNIST_OD_Trainer:
         show_im_scales = np.concatenate([spacer, image_dec0, image_dec, image_dec2], axis=1)
         show_im = np.concatenate([show_im, show_im_gt_scales, show_im_scales])
 
+        show_im = (show_im * 255).astype(np.uint8)
         return show_im
 
     def show_images(self):
@@ -656,5 +758,5 @@ if __name__ == "__main__":
     with tf.Session() as sess:  
         trainer = MNIST_OD_Trainer(sess)
 
-        trainer.show_images()
-        # trainer.train()
+        # trainer.show_images()
+        trainer.train()
